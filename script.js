6a794c14fa82dc5a4e34ee6db9710304dfb82d99
Muthukumar M <muthukumar@kissflow.com>
2024-07-06 21:50:35 +0530

    rewrite of script js

--- script.js
+++ script.js
@@ -1,113 +1,121 @@
-const GRID_DIMENSION = 9;
-const GRID_CELL_DIMENSION = 50;
-const KEYS = {
+const NO_OF_CELLS = 9;
+const CELL_DIMENSION = 50;
+
+const ARROW_KEYS = {
   DOWN: "ArrowDown",
   UP: "ArrowUp",
   LEFT: "ArrowLeft",
   RIGHT: "ArrowRight",
 };
+
 const CLASSES = {
   GRID: "grid",
   HEAD_CELL: "headCell",
 };
-const headPosition = { col: 0, row: 0 };
+
+const INITIAL_HEAD_POSITION = { col: 0, row: 0 };
 
 const generateCellId = (row, col) => `row${row}_col${col}`;
 
 const getCell = (row, col) =>
-  document.querySelector("#" + generateCellId(row, col));
+  document.querySelector(`#${generateCellId(row, col)}`);
 
-function drawGrid(dimension, cellWidth) {
+function createGrid(dimension, cellWidth) {
   const grid = document.createElement("div");
   grid.classList.add("grid");
   grid.style.gridTemplate = `repeat(${dimension},${cellWidth}px) / repeat(${dimension},${cellWidth}px)`;
   for (let row = 0; row < dimension; row++) {
     for (let col = 0; col < dimension; col++) {
-      const cell = getGridCell(-row, col);
+      const cell = createCell(-row, col);
       grid.append(cell);
     }
   }
-  gameDiv.append(grid);
   return grid;
 }
 
-function getGridCell(row, col) {
+function createCell(row, col) {
   const cell = document.createElement("div");
-  //  cell.style.width = width + "px";
   cell.id = generateCellId(row, col);
-  // cell.classList.add("gridCell");
   return cell;
 }
 
 function createHead(row, col) {
-  const headCell = document.querySelector(`#${generateCellId(row, col)}`);
+  const headCell = getCell(row, col);
   headCell.classList.add(CLASSES.HEAD_CELL);
 }
 
-function attachEvent(callback) {
+function attachOnMoveEvent(callback) {
   document.addEventListener("keydown", (e) => {
     const keyCode = e.code;
-    if (Object.values(KEYS).includes(keyCode)) {
-      callback(e, keyCode);
+    const isArrowKeyPressed = Object.values(ARROW_KEYS).includes(keyCode);
+    if (isArrowKeyPressed) {
+      callback(keyCode);
     }
   });
 }
 
-function onMove(currentHeadPosition, direction, event, callback) {
+function onMove(currentHeadPosition, arrowKey, callback) {
   const newHeadPosition = {
     col: currentHeadPosition.col,
     row: currentHeadPosition.row,
   };
-  switch (direction) {
-    case KEYS.DOWN: {
+
+  switch (arrowKey) {
+    case ARROW_KEYS.DOWN: {
       newHeadPosition.row = newHeadPosition.row - 1;
       break;
     }
-    case KEYS.UP: {
+    case ARROW_KEYS.UP: {
       newHeadPosition.row = newHeadPosition.row + 1;
       break;
     }
-    case KEYS.LEFT: {
+    case ARROW_KEYS.LEFT: {
       newHeadPosition.col = newHeadPosition.col - 1;
       break;
     }
-    case KEYS.RIGHT: {
+    case ARROW_KEYS.RIGHT: {
       newHeadPosition.col = newHeadPosition.col + 1;
       break;
     }
   }
+
   newHeadPosition.col =
-    newHeadPosition.col === GRID_DIMENSION
+    newHeadPosition.col === NO_OF_CELLS
       ? 0
       : newHeadPosition.col === -1
-      ? GRID_DIMENSION - 1
+      ? NO_OF_CELLS - 1
       : newHeadPosition.col;
+
   newHeadPosition.row =
-    newHeadPosition.row === -GRID_DIMENSION
+    newHeadPosition.row === -NO_OF_CELLS
       ? 0
       : newHeadPosition.row === 1
-      ? -(GRID_DIMENSION - 1)
+      ? -(NO_OF_CELLS - 1)
       : newHeadPosition.row;
+
   moveHead(currentHeadPosition, newHeadPosition, callback);
 }
 
-function moveHead(fromHeadPosition, toHeadPostion, callback) {
-  const currentHeadCell = getCell(fromHeadPosition.row, fromHeadPosition.col);
+function moveHead(fromPosition, toPosition, callback) {
+  const currentHeadCell = getCell(fromPosition.row, fromPosition.col);
   currentHeadCell.classList.toggle(CLASSES.HEAD_CELL);
-  const movedHeadCell = getCell(toHeadPostion.row, toHeadPostion.col);
+  const movedHeadCell = getCell(toPosition.row, toPosition.col);
   movedHeadCell.classList.toggle(CLASSES.HEAD_CELL);
-  callback(toHeadPostion);
+  callback(toPosition);
 }
+
+const headPosition = INITIAL_HEAD_POSITION;
 
 const gameDiv = document.querySelector("#game");
 
-const grid = drawGrid(GRID_DIMENSION, GRID_CELL_DIMENSION);
+const grid = createGrid(NO_OF_CELLS, CELL_DIMENSION);
+gameDiv.append(grid);
 
 createHead(headPosition.row, headPosition.col);
 
-attachEvent((event, direction) =>
-  onMove(headPosition, direction, event, ({ col, row }) => {
-    headPosition.col = col;
-    headPosition.row = row;
+attachOnMoveEvent((arrowKeyCode) =>
+  onMove(headPosition, arrowKeyCode, (newHeadPosition) => {
+    headPosition.col = newHeadPosition.col;
+    headPosition.row = newHeadPosition.row;
   })
 );
