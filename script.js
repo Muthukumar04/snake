e9971d1ccb66e225abf619a05abc602c4841526d
Muthukumar M <muthukumar@kissflow.com>
2024-07-06 22:44:55 +0530

    moving a snake done

--- script.js
+++ script.js
@@ -11,9 +11,18 @@ const ARROW_KEYS = {
 const CLASSES = {
   GRID: "grid",
   HEAD: "head",
+  BODY: "body",
 };
 
-const INITIAL_HEAD_POSITION = { col: 0, row: 0 };
+function getInitalSnake() {
+  return [
+    { row: 0, col: 4 },
+    { row: 0, col: 3 },
+    { row: 0, col: 2 },
+    { row: 0, col: 1 },
+    { row: 0, col: 0 },
+  ];
+}
 
 const generateCellId = (row, col) => `row${row}_col${col}`;
 
@@ -22,15 +31,17 @@ const getCell = (row, col) =>
 
 function createGrid(dimension, cellWidth) {
   const grid = document.createElement("div");
+  const cells = [];
   grid.classList.add("grid");
   grid.style.gridTemplate = `repeat(${dimension},${cellWidth}px) / repeat(${dimension},${cellWidth}px)`;
   for (let row = 0; row < dimension; row++) {
     for (let col = 0; col < dimension; col++) {
       const cell = createCell(-row, col);
+      cells.push(cell);
       grid.append(cell);
     }
   }
-  return grid;
+  return { grid, cells };
 }
 
 function createCell(row, col) {
@@ -39,9 +50,22 @@ function createCell(row, col) {
   return cell;
 }
 
-function createHead(row, col) {
-  const headCell = getCell(row, col);
-  headCell.classList.add(CLASSES.HEAD);
+function paintSnake(snake, cells, callback = () => {}) {
+  resetGrid(cells);
+  snake.forEach(({ row, col }, index) => {
+    const isHead = index === 0;
+    const cell = getCell(row, col);
+    let className = CLASSES.BODY;
+    if (isHead) {
+      className = CLASSES.HEAD;
+    }
+    cell.className = className;
+  });
+  callback(snake);
+}
+
+function resetGrid(cells) {
+  cells.forEach((cell) => (cell.className = ""));
 }
 
 function attachOnMoveEvent(callback) {
@@ -54,47 +78,64 @@ function attachOnMoveEvent(callback) {
   });
 }
 
-function onMove(currentHeadPosition, arrowKey, callback) {
-  const newHeadPosition = {
-    col: currentHeadPosition.col,
-    row: currentHeadPosition.row,
-  };
+function movePart(part, direction) {
+  const newPart = { ...part };
 
-  switch (arrowKey) {
+  switch (direction) {
     case ARROW_KEYS.DOWN: {
-      newHeadPosition.row = newHeadPosition.row - 1;
+      newPart.row = part.row - 1;
       break;
     }
     case ARROW_KEYS.UP: {
-      newHeadPosition.row = newHeadPosition.row + 1;
+      newPart.row = part.row + 1;
       break;
     }
     case ARROW_KEYS.LEFT: {
-      newHeadPosition.col = newHeadPosition.col - 1;
+      newPart.col = part.col - 1;
       break;
     }
     case ARROW_KEYS.RIGHT: {
-      newHeadPosition.col = newHeadPosition.col + 1;
+      newPart.col = part.col + 1;
       break;
     }
   }
 
-  newHeadPosition.col =
-    newHeadPosition.col === NO_OF_CELLS
+  newPart.col =
+    newPart.col === NO_OF_CELLS
       ? 0
-      : newHeadPosition.col === -1
+      : newPart.col === -1
       ? NO_OF_CELLS - 1
-      : newHeadPosition.col;
+      : newPart.col;
 
-  newHeadPosition.row =
-    newHeadPosition.row === -NO_OF_CELLS
+  newPart.row =
+    newPart.row === -NO_OF_CELLS
       ? 0
-      : newHeadPosition.row === 1
+      : newPart.row === 1
       ? -(NO_OF_CELLS - 1)
-      : newHeadPosition.row;
+      : newPart.row;
 
-  moveHead(currentHeadPosition, newHeadPosition, callback);
+  return newPart;
 }
+
+function onMove(currentSnake = [], direction, cells, callback) {
+  const updatedSnake = currentSnake.map((part, index) => {
+    if (index === 0) {
+      const updatedHead = movePart(part, direction);
+      if (willCollide(updatedHead, currentSnake)) {
+        alert("Colliding");
+        paintSnake(getInitalSnake(), cells);
+      }
+      return updatedHead;
+    } else {
+      return currentSnake[index - 1];
+    }
+  });
+
+  paintSnake(updatedSnake, cells, callback);
+}
+
+const willCollide = (head, snake) =>
+  snake.find((part) => part.col === head.col && part.row === head.row);
 
 function moveHead(fromPosition, toPosition, callback) {
   const currentHeadCell = getCell(fromPosition.row, fromPosition.col);
@@ -104,18 +145,15 @@ function moveHead(fromPosition, toPosition, callback) {
   callback(toPosition);
 }
 
-const headPosition = INITIAL_HEAD_POSITION;
+let snake = getInitalSnake();
 
 const gameDiv = document.querySelector("#game");
 
-const grid = createGrid(NO_OF_CELLS, CELL_DIMENSION);
+const { grid, cells } = createGrid(NO_OF_CELLS, CELL_DIMENSION);
 gameDiv.append(grid);
 
-createHead(headPosition.row, headPosition.col);
+paintSnake(snake, cells);
 
-attachOnMoveEvent((arrowKeyCode) =>
-  onMove(headPosition, arrowKeyCode, (newHeadPosition) => {
-    headPosition.col = newHeadPosition.col;
-    headPosition.row = newHeadPosition.row;
-  })
+attachOnMoveEvent((direction) =>
+  onMove(snake, direction, cells, (updatedSnake) => (snake = updatedSnake))
 );
